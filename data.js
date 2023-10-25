d353be05765ace5ff3e6a45586ef9807db03561e
sameer srivastava <srivastavasameer2002@gmail.com>
2023-10-25 16:58:31 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,22 @@
+const songs=[
+    {songName: 'Heeriye', filePath: 'song/1.mp3', coverPath: 'cover/1.jpg'},
+    {songName: 'Kalastar', filePath: 'song/2.mp3', coverPath: 'cover/2.jpg'},
+    {songName: 'Kya Loge Tum', filePath: 'song/3.mp3', coverPath: 'cover/3.jpg'},
+    {songName: 'Maan Meri Jaan', filePath: 'song/4.mp3', coverPath: 'cover/4.jpg'},
+    {songName: 'Phir Aur Kya Chahiye', filePath: 'song/5.mp3', coverPath: 'cover/5.jpg'},
+    {songName: 'Pyaar Hota Kayi Baar Hai', filePath: 'song/6.mp3', coverPath: 'cover/6.jpg'},
+    {songName: 'Savage -YOYO Honey Singh', filePath: 'song/7.mp3', coverPath: 'cover/7.jpg'},
+    {songName: 'Tere Pyar Mein', filePath: 'song/8.mp3', coverPath: 'cover/8.jpg'},    
+    {songName: 'Tune Jo Na Kaha', filePath: 'song/9.mp3', coverPath: 'cover/9.jpg'},
+    {songName: 'Raam Siya Raam', filePath: 'song/10.mp3', coverPath: 'cover/10.jpg'},
+    {songName: 'Heeriye', filePath: 'song/11.mp3', coverPath: 'cover/11.jpg'},
+    {songName: 'Kalastar', filePath: 'song/12.mp3', coverPath: 'cover/12.jpg'},
+    {songName: 'Kya Loge Tum', filePath: 'song/13.mp3', coverPath: 'cover/13.jpg'},
+    {songName: 'Maan Meri Jaan', filePath: 'song/14.mp3', coverPath: 'cover/14.jpg'},
+    {songName: 'Phir Aur Kya Chahiye', filePath: 'song/15.mp3', coverPath: 'cover/15.jpg'},
+    {songName: 'Pyaar Hota Kayi Baar Hai', filePath: 'song/16.mp3', coverPath: 'cover/16.jpg'},
+    {songName: 'Savage -YOYO Honey Singh', filePath: 'song/17.mp3', coverPath: 'cover/17.jpg'},
+    {songName: 'Tere Pyar Mein', filePath: 'song/18.mp3', coverPath: 'cover/18.jpg'},    
+    {songName: 'Tune Jo Na Kaha', filePath: 'song/19.mp3', coverPath: 'cover/19.jpg'},
+    {songName: 'Raam Siya Raam', filePath: 'song/20.mp3', coverPath: 'cover/20.jpg'}
+];
--- script.js
+++ script.js
@@ -1,16 +1,4 @@
 console.log("WELCOME TO GUNGUNAO");
-const songs=[
-    {songName: 'Heeriye', filePath: 'song/1.mp3', coverPath: 'cover/1.jpg'},
-    {songName: 'Kalastar', filePath: 'song/2.mp3', coverPath: 'cover/2.jpg'},
-    {songName: 'Kya Loge Tum', filePath: 'song/3.mp3', coverPath: 'cover/3.jpg'},
-    {songName: 'Maan Meri Jaan', filePath: 'song/4.mp3', coverPath: 'cover/4.jpg'},
-    {songName: 'Phir Aur Kya Chahiye', filePath: 'song/5.mp3', coverPath: 'cover/5.jpg'},
-    {songName: 'Pyaar Hota Kayi Baar Hai', filePath: 'song/6.mp3', coverPath: 'cover/6.jpg'},
-    {songName: 'Savage -YOYO Honey Singh', filePath: 'song/7.mp3', coverPath: 'cover/7.jpg'},
-    {songName: 'Tere Pyar Mein', filePath: 'song/8.mp3', coverPath: 'cover/8.jpg'},    
-    {songName: 'Tune Jo Na Kaha', filePath: 'song/9.mp3', coverPath: 'cover/9.jpg'},
-    {songName: 'Raam Siya Raam', filePath: 'song/10.mp3', coverPath: 'cover/10.jpg'}
-];
 let html='';
 songs.forEach((element,i)=>{
     html+=`<div class="songItem">
